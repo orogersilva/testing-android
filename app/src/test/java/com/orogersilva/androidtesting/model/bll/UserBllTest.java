9df3c49a15dc0da68d9fc1700b05f049c587f4bc
Roger Silva <orogersilva@gmail.com>
2016-01-10 14:37:14 -0200

    Implementação de mais alguns métodos de teste em UserBllTest.

--- app/src/test/java/com/orogersilva/androidtesting/model/bll/UserBllTest.java
+++ app/src/test/java/com/orogersilva/androidtesting/model/bll/UserBllTest.java
@@ -10,6 +10,9 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
 /**
  * Created by azevedor on 10/01/2016.
  */
@@ -36,6 +39,90 @@ public class UserBllTest {
 
         // ACT
         userBll.addUser(nullUser);
+    }
+
+    @Test
+    @SmallTest
+    public void addUser_whenUserIsNotNull_verifyCallToDal() {
+
+        // ARRANGE
+
+        final String USER_NAME = "Roger";
+        final String USER_AGE = "27";
+        final String USER_CITY = "Alvorada";
+
+        User validUser = new User(USER_NAME, USER_AGE, USER_CITY);
+
+        UserBll userBll = new UserBll(mUserDalMock);
+
+        // ACT
+        userBll.addUser(validUser);
+
+        // ASSERT
+        verify(mUserDalMock, times(1)).createUser(validUser);
+    }
+
+    @Test
+    @SmallTest
+    public void getUser_whenUserNameIsNull_returnsNull() {
+
+        // ARRANGE
+
+        final String NULL_USER_NAME = null;
+
+        when(mUserDalMock.retrieveUser(NULL_USER_NAME)).thenReturn(null);
+
+        UserBll userBll = new UserBll(mUserDalMock);
+
+        // ACT
+        User gottenNullUser = userBll.getUser(NULL_USER_NAME);
+
+        // ASSERT
+        assertNull(gottenNullUser);
+    }
+
+    @Test
+    @SmallTest
+    public void getUser_whenUserNameIsEmpty_returnsNull() {
+
+        // ARRANGE
+
+        final String EMPTY_USER_NAME = "";
+
+        when(mUserDalMock.retrieveUser(EMPTY_USER_NAME)).thenReturn(null);
+
+        UserBll userBll = new UserBll(mUserDalMock);
+
+        // ACT
+        User gottenNullUser = userBll.getUser(EMPTY_USER_NAME);
+
+        // ASSERT
+        assertNull(gottenNullUser);
+    }
+
+    @Test
+    @SmallTest
+    public void getUser_whenUserNameIsValid_returnsUser() {
+
+        // ARRANGE
+
+        final String FAILED_TEST_MESSAGE = "Usuário recuperado deve ser igual ao usuário esperado.";
+
+        final String VALID_USER_NAME = "Roger";
+        final String USER_AGE = "27";
+        final String USER_CITY = "Alvorada";
+
+        User expectedUser = new User(VALID_USER_NAME, USER_AGE, USER_CITY);
+
+        when(mUserDalMock.retrieveUser(VALID_USER_NAME)).thenReturn(expectedUser);
+
+        UserBll userBll = new UserBll(mUserDalMock);
+
+        // ACT
+        User gottenValidUser = userBll.getUser(VALID_USER_NAME);
+
+        // ASSERT
+        assertEquals(FAILED_TEST_MESSAGE, expectedUser, gottenValidUser);
     }
 
     // endregion
